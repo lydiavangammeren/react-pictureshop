f1a1ba39b042d83869c66707c16a96cf49b4c9ed
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2021-09-27 20:07:38 +0200

    Add files via upload

---
+++ src/CartItem.js
@@ -0,0 +1,33 @@
+import React, {useContext} from "react"
+import PropTypes from "prop-types"
+import {Context} from "../src/Context"
+import useHover from "../src/hooks/useHover"
+
+function CartItem({item}) {
+    const [hovered, ref] = useHover()
+    const {removeFromCart} = useContext(Context)
+    
+    const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
+    
+    return (
+        <div className="cart-item">
+            <i 
+                className={iconClassName} 
+                onClick={() => removeFromCart(item.id)}
+                ref={ref}
+            >
+            </i>
+            
+            <img alt="" src={item.url} width="130px" />
+            <p>$5.99</p>
+        </div>
+    )
+}
+
+CartItem.propTypes = {
+    item: PropTypes.shape({
+        url: PropTypes.string.isRequired
+    })
+}
+
+export default CartItem
